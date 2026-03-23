c6262af489944691d9e8a8211c1702dfccf65814
Pedro Ferreira Rocha <pedroferreirarocha10@gmail.com>
2026-03-23 16:42:40 +0000

    desafio nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,74 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  // Estado de A a H.
+  char EstadoA;
+  char EstadoB;
+  // Código da carta de 01 a 04.
+  char CodigoA[4];
+  char CodigoB[4];
+  // Nome da cidade
+  char NomeA[20];
+  char NomeB[20];
+  // População.
+  int PopulacaoA;
+  int PopulacaoB;
+  // Área(Em km²).
+  float AreaA;
+  float AreaB;
+  //PIB da cidade.
+  float PibA;
+  float PibB;
+  //Número de Pontos Turísticos na cidade
+  int PontosTuristicosA;
+  int PontosTuristicosB;
   // Área para entrada de dados
+  //carta A
+  printf("Qual a letra do estado da carta A de A a H: ");
+  scanf(" %c", &EstadoA);
 
+  printf("Qual a codigo da carta A de 01 a 04: %c", EstadoA);
+  scanf("%s", CodigoA);
+  //não bote cidade com espaço
+  printf("Qual o nome da cidade da carta A: ");
+  scanf("%s", NomeA);
+
+  printf("Qual a população da carta A: ");
+  scanf("%d", &PopulacaoA);
+
+  printf("Qual a área da carta A: ");
+  scanf("%f", &AreaA);
+
+  printf("Qual o PIB da carta A: ");
+  scanf("%f", &PibA);
+
+  printf("Quantos pontos turisticos tem a carta A: ");
+  scanf("%d", &PontosTuristicosA);
+  //carta B
+  printf("Qual a letra do estado da carta B de A a H: ");
+  scanf(" %c", &EstadoB);
+
+  printf("Qual a codigo da carta B de 01 a 04: %c", EstadoB);
+  scanf("%s", CodigoB);
+  //não bote cidade com espaço
+  printf("Qual o nome da cidade da carta B: ");
+  scanf("%s", NomeB);
+
+  printf("Qual a população da carta B: ");
+  scanf("%d", &PopulacaoB);
+
+  printf("Qual a área da carta B: ");
+  scanf("%f", &AreaB);
+
+  printf("Qual o PIB da carta B: ");
+  scanf("%f", &PibB);
+
+  printf("Quantos pontos turisticos tem a carta B: ");
+  scanf("%d", &PontosTuristicosB);
   // Área para exibição dos dados da cidade
-
+  //carta A
+  printf("A carta A:\n Estado: %c \n Codigo: %c%s \n Nome: %s \n população: %d \n área: %f \n PIB: %f \n pontos turisticos: %d \n",EstadoA, EstadoA, CodigoA, NomeA, PopulacaoA, AreaA, PibA, PontosTuristicosA);
+  //carta B
+  printf("A carta B:\n Estado: %c \n Codigo: %c%s \n Nome: %s \n população: %d \n área: %f \n PIB: %f \n pontos turisticos: %d \n",EstadoB, EstadoB, CodigoB, NomeB, PopulacaoB, AreaB, PibB, PontosTuristicosB);
 return 0;
 } 
